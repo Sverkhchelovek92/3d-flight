3314fbce639ef5adfdb9df33e9e19d3024bd180c
Miroslav Gurnyak <mirmiroslav@gmail.com>
2025-11-15 20:38:42 +0300

    some preparations

---
+++ script.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById('matrix')
+const ctx = canvas.getContext('2d')
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+}
+
+resizeCanvas()
+window.addEventListener('resize', resizeCanvas)
